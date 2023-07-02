39b950300beb922929520366c99100d7e99636cb
Gift <geezyenyoghasim@gmail.com>
2023-07-02 23:20:33 +0100

    signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,32 @@ const { Router } = require("express");
 
 const router = Router();
 
+const authMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      message: "You must be logged in",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        message: "You must be logged in",
+      });
+    }
+
+    req.user = decoded;
+
+    return next();
+  });
+};
+
 router.use("/auth", require("./auth"));
+
+router.use("/user", authMiddleware, require("./user"));
 
 module.exports = router;
---
+++ src/routes/user.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.use("/", (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({
+      message: "You must be logged in",
+    });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const userDetails = await Users.findById(req.user._id);
+
+    return res.status(200).json({
+      message: "User details retrieved successfully",
+      data: {
+        fullname: userDetails.fullname,
+        email: userDetails.email,
+        phoneNumber: userDetails.phoneNumber,
+        address: userDetails.address,
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error retrieving user details",
+      error: err.message,
+    });
+  }
+});
+
+module.exports = router;
